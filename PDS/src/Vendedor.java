31b6efe15b93bf17e197986d4ce55fc31f634364
Nicolas Lussoro <lussoro8@gmail.com>
2025-05-25 17:49:46 -0300

    añado implementacion de clases

--- PDS/src/Vendedor.java
+++ PDS/src/Vendedor.java
@@ -2,4 +2,14 @@ public class Vendedor {
     private String nombre;
     private String apellido;
     // getters, setters, constructor
+    public Vendedor(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
 }
