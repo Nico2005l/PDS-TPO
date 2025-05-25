4da7bab1d9e585da8d89a8b2b599d933d6b1699d
Tomas Liñeira Abal <tlineira@gmail.com>
2025-05-25 17:43:03 -0300

    setters

--- PDS/src/Vendedor.java
+++ PDS/src/Vendedor.java
@@ -1,5 +1,10 @@
 public class Vendedor {
     private String nombre;
     private String apellido;
+
+    public Vendedor(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
     // getters, setters, constructor
 }
