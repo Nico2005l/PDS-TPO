019545e8f86de973e5e972cc57a883ede4650ba4
Erik Berns <erik.christian.berns@gmail.com>
2025-06-20 20:04:11 -0300

    FacadeVendedor

--- PDS/src/FacadeVendedor.java
+++ PDS/src/FacadeVendedor.java
@@ -1,5 +1,21 @@
+import java.util.List;
+
 public class FacadeVendedor {
-    public void verVehiculosDisponibles() {
-        // Mostrar vehículos que no estén en proceso de venta
+    private Sistema sis;
+
+    public FacadeVendedor(Sistema sistema) {
+        this.sis = sistema;
     }
+
+    /**
+     * Muestra todos los vehículos disponibles en el catálogo.
+     */
+    public void verVehiculosDisponibles() {
+        List<Vehiculo> catalogo = sis.getCatalogoVehiculos();
+        System.out.println("Catálogo de vehículos disponibles:");
+        for (Vehiculo v : catalogo) {
+            System.out.printf("- %s %s ($%d)%n", v.getClass().getSimpleName(), v.toString(), v.getCosto());
+        }
+    }
+
 }
