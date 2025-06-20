e69ec8ac30ffc4fcf6102ef994a616a794078bb2
Tomas Liñeira Abal <tlineira@gmail.com>
2025-06-20 20:13:59 -0300

    v

--- PDS/src/DetallePedido.java
+++ PDS/src/DetallePedido.java
@@ -40,10 +40,6 @@ public class DetallePedido {
         this.cliente = cliente;
     }
 
-    public Cliente getCliente() {
-        return cliente;
-    }
-
     public void setVendedor(Vendedor vendedor) {
         this.vendedor = vendedor;
     }
