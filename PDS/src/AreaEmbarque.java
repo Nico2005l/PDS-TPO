6941f24c8b6140c330250c86f97c2bf46d991684
Erik Berns <erik.christian.berns@gmail.com>
2025-06-20 19:15:47 -0300

    Embarque

--- PDS/src/AreaEmbarque.java
+++ PDS/src/AreaEmbarque.java
@@ -1,3 +1,13 @@
-public class AreaEmbarque {
-    
+public class AreaEmbarque implements Area{
+
+    @Override
+    public void procesarPedido(PedidoCompra p) {
+       
+    }
+
+    @Override
+    public void actualizarEstado() {
+        
+    }
+  
 }
